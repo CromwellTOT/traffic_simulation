9ad007845fcd50c7200d41c3427e8f7f0940bb4b
Frank Zhang <fzhang@xandr.com>
2021-04-11 23:34:46 -0400

    add UI console panel

--- bundle.js
+++ bundle.js
@@ -161,12 +161,17 @@ const Road = require('./app/Road');
 //const config = require('./config');
 const framePerSecond = 5;
 const interval = 1000 / framePerSecond;
+
 const timerElem = document.getElementById("timer");
+
+let INTERVAL;
+let timer; // second
 
 const road = new Road();
 
-let timer = 0; // second
 
+
+// put 4 cars on the board
 road.generateCar();
 road.generateCar(100,10);
 road.generateCar(50, 1);
@@ -180,11 +185,45 @@ function simulate() {
     road.render();
     road.calculate(interval / 1000);
 
-    if (timer >= 100 || !road.hasCars()) {
-        console.log('stopped');
-        clearInterval(INTERVAL);
+    if (timer >= 10 || !road.hasCars()) {
+        stop();
     }
 }
 
-const INTERVAL = setInterval(simulate, interval);
+restart();
+// controllers
+function restart() {
+    stop();
+    timer = 0;
+    start();
+}
+
+function stop() {
+    console.log('stopped');
+    clearInterval(INTERVAL);
+}
+
+function start() {
+    // to prevent race
+    if (INTERVAL) {
+        clearInterval(INTERVAL);
+    }
+    INTERVAL = setInterval(simulate, interval);
+}
+// even listeners from console panel
+document.getElementById('stopresume').addEventListener('click', (event) => {
+    const thisButton = event.target;
+
+    if (thisButton.innerHTML === 'resume') {
+        start();
+        thisButton.innerHTML = "stop";
+    } else if (thisButton.innerHTML === 'stop') {
+        stop();
+        thisButton.innerHTML = "resume";
+    }
+});
+document.getElementById('restart').addEventListener('click', () => {
+    restart();
+    document.getElementById('stopresume').innerHTML = 'stop';
+});
 },{"./app/Road":2}]},{},[4]);
